0c0eebcebb8568ee66026da9d7373113a2bf6cd3
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-07 23:49:23 -0500

    commit tree c6e4678eeac27250a9b626944110e00890a378da

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {name:"Sam" , streetAddress:"Toronto"};
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value){
+
+    return{
+        ...employeeObj,
+        [key]:value,
+    };
+
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value){
+
+   employeeObj[key]=value;
+
+   return employeeObj
+}
+
+function deleteFromEmployeeByKey(employeeObj, key){
+    const newEmployeeObj = {...employeeObj}
+    
+    delete newEmployeeObj[key];
+
+    return newEmployeeObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key){
+
+    delete employeeObj[key];
+    return employeeObj
+}
